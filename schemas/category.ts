0d24a2212ae173976f450768755cb567dea54a2b
Ahmad Sorour <ahmad_sorour@outlook.com>
2022-12-01 13:02:33 +0200

    added folder icon to category schema

--- schemas/category.ts
+++ schemas/category.ts
@@ -1,8 +1,10 @@
+import { FolderIcon } from '@sanity/icons'
 import { defineField, defineType } from 'sanity'
 
 export default defineType({
   name: 'category',
   title: 'Category',
+  icon: FolderIcon,
   type: 'document',
   fields: [
     defineField({
